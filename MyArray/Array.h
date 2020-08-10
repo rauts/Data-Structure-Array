62f9e2aaf85dd456480a29ab5cf9e9cc8e1e5141
Sunil Raut <sunil.rautkshetri@gmail.com>
2020-08-10 15:40:46 +0200

    Add constructors, a destructor, and 3 public member functions in Array.h
    Three added pubilc member functions are:
    int Size() const;
    bool isEmpty() const;
    void PrintArray() const;

--- MyArray/Array.h
+++ MyArray/Array.h
@@ -1,5 +1,36 @@
 #pragma once
+// First create a class for integer type data. Once it works with integer data
+// type then change the class to class template for generic data type.
+// 
 class Array
 {
+private:
+	
+	// declare data members
+	
+	int mSize{};
+	int* mPtr{ nullptr };
+
+public:
+	
+	// default constructor
+	
+	Array() = default;
+
+	// parameterized constructor
+
+	Array(int size);
+
+	// destructor
+
+	~Array();
+
+		 
+	// define public interface
+	
+	int Size() const;
+	bool isEmpty() const;
+	void PrintArray() const;
+
 };
 
